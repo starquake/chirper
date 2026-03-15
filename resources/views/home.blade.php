88f4eda2ea43c1501d46127d323392c07d10e274
Jan Visser <starquake@linuxeverywhere.org>
2026-03-15 20:50:00 +0100

    Followed some parts of the tutorial

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,23 @@
+
 <x-layout>
     <x-slot:title>
         Welcome
     </x-slot:title>
     <div class="max-w-2xl mx-auto">
-        <div class="card bg-base-100 shadow mt-8">
-            <div class="card-body">
-                <div>
-                    <h1 class="text-3xl font-bold">Welcome to Chirper!</h1>
-                    <p>This is your brand new Laravel app. Time to make it sing (or chirp)!</p>
-                    <p class="mt-2 text-sm text-gray-600">Now this is live on the internet! 🎉</p>
+        @forelse ($chirps as $chirp)
+            <div class="card bg-base-100 shadow mt-8">
+                <div class="card-body">
+                    <div>
+                        <div class="font-semibold"> {{ $chirp->user ? $chirp->user->name : 'Anonymous' }}</div>
+                        <div class="mt-1">{{ $chirp->message }}</div>
+                        <div class="text-sm text-gray-500 mt-2">
+                            {{ $chirp->created_at->diffForHumans() }}
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        @empty
+            <p class="text-gray-500">No chirps yet. Be the first to chirp!</p>
+        @endforelse
     </div>
 </x-layout>
